689c382bdc4dc010bb8da96eaf2e0fec6aed2711
Jeremias Rauwolf <jeremias.rauwolf123@gmail.com>
2023-10-20 16:42:21 +0200

    fix async loadmodule

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -3,7 +3,10 @@ import '../styles/styles.scss'
 if(document.querySelector('[data-loadmodule]')) {
     const module : HTMLElement | null = document.querySelector('[data-loadmodule]')
     const moduleName : string | undefined = module?.dataset.loadmodule
+    loadmodule(moduleName)
+}
+
+async function loadmodule(moduleName : string | undefined) {
     const importedModule = await import(`./modules/${moduleName}.ts`)
     importedModule.default()
 }
-
